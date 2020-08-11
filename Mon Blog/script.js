5cea61abc0eabb6762608b9321f469c461b536cd
rayan-a-07 <68562124+rayan-a-07@users.noreply.github.com>
2020-08-11 22:59:16 +0000

    Add files via upload

---
+++ Mon Blog/script.js
@@ -0,0 +1,5 @@
+var fd_post_img = document.createElement('img')
+fd_post_img.src = '/img/book.png'
+var fd_post_img_container = document.getElementsByClassName('featured-post-img')[0]
+fd_post_img_container.appendChild(fd_post_img)
+
